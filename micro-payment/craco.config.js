6c79d243e9fb1e6ad1936ab7d814605d32771eaa
omar star <obarakat12@gmail.com>
2021-11-10 16:54:14 +0200

    mfe with craco config

--- micro-payment/craco.config.js
+++ micro-payment/craco.config.js
@@ -5,4 +5,25 @@ module.exports = {
       plugins: [require("tailwindcss"), require("autoprefixer")],
     },
   },
+  webpack: {
+    configure: (webpackConfig, { env, paths }) => {
+      webpackConfig.optimization.splitChunks = {
+        chunks(chunk) {
+          return false;
+        },
+      };
+      webpackConfig.optimization.runtimeChunk = false;
+      webpackConfig.output.filename = "static/js/micro-payment.js";
+      return webpackConfig;
+    },
+  },
+  devServer: {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers":
+        "X-Requested-With, content-type, Authorization",
+      "Access-Control-Allow-Credentials": "true",
+    },
+  },
 };
